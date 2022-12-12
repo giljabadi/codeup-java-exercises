4a4dd42b4c638a4fe6af843edd4e9c01723f239d
Gil Jacob Abadi <giljabadi@gmail.com>
2022-12-12 10:06:59 -0600

    java first exercise

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 18;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Top o the mornin";
+        System.out.println(myString);
+
+//        float myNumber = 3.14;
+//        myNumber = 123;
+//        System.out.println(myNumber);
+
+
+    }
+}
